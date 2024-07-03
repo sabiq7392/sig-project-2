603901dcb40bcde051b84828f293edefaf012dc9
sabiq7392 <sabiqmuhammad98@gmail.com>
2024-07-03 18:36:05 +0700

    init

---
+++ peta.data.js
@@ -0,0 +1,482 @@
+const petaData = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 11,
+        "nama": "Nanggroe Aceh Darussalam",
+        "deskripsi": "Aceh adalah sebuah provinsi di Indonesia yang terletak pada bagian utara Pulau Sumatra dengan ibu kota di Banda Aceh. Aceh merupakan salah satu daerah istimewa dan diberi kewenangan otonomi khusus. Aceh terletak di ujung utara pulau Sumatra dan merupakan provinsi paling barat di Indonesia.",
+        "nama_gubernur": "Nova Iriansyah",
+        "wisata_favorit": "Museum Tsunami Aceh"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [95.3162, 5.5483]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 12,
+        "nama": "Sumatera Utara",
+        "deskripsi": "Sumatera Utara adalah sebuah provinsi di Indonesia yang terletak di bagian utara Pulau Sumatera. Ibu kotanya adalah Medan.",
+        "nama_gubernur": "Edy Rahmayadi",
+        "wisata_favorit": "Danau Toba"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [98.6699, 3.5952]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 13,
+        "nama": "Sumatera Barat",
+        "deskripsi": "Sumatera Barat adalah sebuah provinsi di Indonesia yang terletak di bagian barat Pulau Sumatera. Ibu kotanya adalah Padang.",
+        "nama_gubernur": "Mahyeldi Ansharullah",
+        "wisata_favorit": "Jam Gadang"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [100.3692, -0.9471]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 14,
+        "nama": "Riau",
+        "deskripsi": "Riau adalah sebuah provinsi di Indonesia yang terletak di bagian tengah Pulau Sumatera. Ibu kotanya adalah Pekanbaru.",
+        "nama_gubernur": "Syamsuar",
+        "wisata_favorit": "Pulau Penyengat"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [101.4475, 0.5071]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 15,
+        "nama": "Kepulauan Riau",
+        "deskripsi": "Kepulauan Riau adalah sebuah provinsi di Indonesia yang terletak di bagian barat Indonesia. Ibu kotanya adalah Tanjung Pinang.",
+        "nama_gubernur": "Ansar Ahmad",
+        "wisata_favorit": "Pulau Bintan"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [104.5395, 0.9158]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 16,
+        "nama": "Jambi",
+        "deskripsi": "Jambi adalah sebuah provinsi di Indonesia yang terletak di bagian tengah Pulau Sumatera. Ibu kotanya adalah Jambi.",
+        "nama_gubernur": "Al Haris",
+        "wisata_favorit": "Taman Nasional Bukit Duabelas"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [103.6167, -1.6106]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 17,
+        "nama": "Sumatera Selatan",
+        "deskripsi": "Sumatera Selatan adalah sebuah provinsi di Indonesia yang terletak di bagian selatan Pulau Sumatera. Ibu kotanya adalah Palembang.",
+        "nama_gubernur": "Herman Deru",
+        "wisata_favorit": "Benteng Kuto Besak"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [104.7458, -2.9761]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 18,
+        "nama": "Bengkulu",
+        "deskripsi": "Bengkulu adalah sebuah provinsi di Indonesia yang terletak di bagian barat daya Pulau Sumatera. Ibu kotanya adalah Bengkulu.",
+        "nama_gubernur": "Rohidin Mersyah",
+        "wisata_favorit": "Pulau Tikus"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [102.2655, -3.8004]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 19,
+        "nama": "Lampung",
+        "deskripsi": "Lampung adalah sebuah provinsi di Indonesia yang terletak di bagian selatan Pulau Sumatera. Ibu kotanya adalah Bandar Lampung.",
+        "nama_gubernur": "Arinal Djunaidi",
+        "wisata_favorit": "Pantai Kiluan"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [105.2580, -5.4283]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 21,
+        "nama": "Kepulauan Bangka Belitung",
+        "deskripsi": "Kepulauan Bangka Belitung adalah sebuah provinsi di Indonesia yang terletak di bagian timur Pulau Sumatera. Ibu kotanya adalah Pangkal Pinang.",
+        "nama_gubernur": "Erzaldi Rosman Djohan",
+        "wisata_favorit": "Pulau Lengkuas"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [106.1175, -2.1295]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 31,
+        "nama": "DKI Jakarta",
+        "deskripsi": "Jakarta adalah ibu kota negara dan kota terbesar di Indonesia, terletak di pulau Jawa. Jakarta merupakan pusat politik, ekonomi, dan budaya Indonesia.",
+        "nama_gubernur": "Anies Baswedan",
+        "wisata_favorit": "Monumen Nasional (Monas)"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [106.8272, -6.1751]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 32,
+        "nama": "Jawa Barat",
+        "deskripsi": "Jawa Barat adalah sebuah provinsi di Indonesia yang terletak di bagian barat Pulau Jawa. Ibu kotanya adalah Bandung.",
+        "nama_gubernur": "Ridwan Kamil",
+        "wisata_favorit": "Kawah Putih"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [107.6191, -6.9175]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 33,
+        "nama": "Jawa Tengah",
+        "deskripsi": "Jawa Tengah adalah sebuah provinsi di Indonesia yang terletak di bagian tengah Pulau Jawa. Ibu kotanya adalah Semarang.",
+        "nama_gubernur": "Ganjar Pranowo",
+        "wisata_favorit": "Candi Borobudur"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [110.4188, -7.1509]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 34,
+        "nama": "DI Yogyakarta",
+        "deskripsi": "DI Yogyakarta adalah sebuah provinsi di Indonesia yang memiliki status istimewa. Ibu kotanya adalah Yogyakarta.",
+        "nama_gubernur": "Sri Sultan Hamengkubuwono X",
+        "wisata_favorit": "Candi Prambanan"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [110.3874, -7.7956]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 35,
+        "nama": "Jawa Timur",
+        "deskripsi": "Jawa Timur adalah sebuah provinsi di Indonesia yang terletak di bagian timur Pulau Jawa. Ibu kotanya adalah Surabaya.",
+        "nama_gubernur": "Khofifah Indar Parawansa",
+        "wisata_favorit": "Gunung Bromo"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [112.7325, -7.2751]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 36,
+        "nama": "Banten",
+        "deskripsi": "Banten adalah sebuah provinsi di Indonesia yang terletak di bagian barat Pulau Jawa. Ibu kotanya adalah Serang.",
+        "nama_gubernur": "Banten memiliki status istimewa sebagai provinsi berdasarkan Keputusan Presiden RI No. 18 tahun 2022, tanggal 30 Maret 2022 oleh Jokowi.",
+        "wisata_favorit": "Pantai Anyer"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [106.1092, -6.4058]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 51,
+        "nama": "Bali",
+        "deskripsi": "Bali adalah sebuah provinsi di Indonesia yang terletak di bagian selatan Kepulauan Sunda Kecil. Ibu kotanya adalah Denpasar.",
+        "nama_gubernur": "I Wayan Koster",
+        "wisata_favorit": "Pantai Kuta"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [115.1889, -8.4095]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 52,
+        "nama": "Nusa Tenggara Barat",
+        "deskripsi": "Nusa Tenggara Barat adalah sebuah provinsi di Indonesia yang terletak di bagian barat Kepulauan Nusa Tenggara. Ibu kotanya adalah Mataram.",
+        "nama_gubernur": "Zulkieflimansyah",
+        "wisata_favorit": "Gili Trawangan"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [116.1167, -8.6500]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 53,
+        "nama": "Nusa Tenggara Timur",
+        "deskripsi": "Nusa Tenggara Timur adalah sebuah provinsi di Indonesia yang terletak di bagian timur Kepulauan Nusa Tenggara. Ibu kotanya adalah Kupang.",
+        "nama_gubernur": "Viktor Laiskodat",
+        "wisata_favorit": "Pulau Komodo"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [123.5833, -9.1167]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 61,
+        "nama": "Kalimantan Barat",
+        "deskripsi": "Kalimantan Barat adalah sebuah provinsi di Indonesia yang terletak di bagian barat Pulau Kalimantan. Ibu kotanya adalah Pontianak.",
+        "nama_gubernur": "Sutarmidji",
+        "wisata_favorit": "Kubah Kayu"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [109.3333, -0.0333]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 62,
+        "nama": "Kalimantan Tengah",
+        "deskripsi": "Kalimantan Tengah adalah sebuah provinsi di Indonesia yang terletak di bagian tengah Pulau Kalimantan. Ibu kotanya adalah Palangkaraya.",
+        "nama_gubernur": "Sugianto Sabran",
+        "wisata_favorit": "Taman Nasional Tanjung Puting"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [113.5833, -1.5167]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 63,
+        "nama": "Kalimantan Selatan",
+        "deskripsi": "Kalimantan Selatan adalah sebuah provinsi di Indonesia yang terletak di bagian selatan Pulau Kalimantan. Ibu kotanya adalah Banjarmasin.",
+        "nama_gubernur": "H. Sahbirin Noor",
+        "wisata_favorit": "Pulau Laut"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [114.5910, -3.3199]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 64,
+        "nama": "Kalimantan Timur",
+        "deskripsi": "Kalimantan Timur adalah sebuah provinsi di Indonesia yang terletak di bagian timur Pulau Kalimantan. Ibu kotanya adalah Samarinda.",
+        "nama_gubernur": "Awang Faroek Ishak",
+        "wisata_favorit": "Derawan"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [117.1500, -0.5000]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 65,
+        "nama": "Kalimantan Utara",
+        "deskripsi": "Kalimantan Utara adalah sebuah provinsi di Indonesia yang terletak di bagian utara Pulau Kalimantan. Ibu kotanya adalah Tanjung Selor.",
+        "nama_gubernur": "Irianto Lambrie",
+        "wisata_favorit": "Taman Nasional Bukit Baka Bukit Raya"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [117.4193, 2.3255]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 71,
+        "nama": "Sulawesi Utara",
+        "deskripsi": "Sulawesi Utara adalah sebuah provinsi di Indonesia yang terletak di bagian utara Pulau Sulawesi. Ibu kotanya adalah Manado.",
+        "nama_gubernur": "Olly Dondokambey",
+        "wisata_favorit": "Bunaken"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [124.8935, 1.5016]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 72,
+        "nama": "Sulawesi Tengah",
+        "deskripsi": "Sulawesi Tengah adalah sebuah provinsi di Indonesia yang terletak di bagian tengah Pulau Sulawesi. Ibu kotanya adalah Palu.",
+        "nama_gubernur": "Rusdy Mastura",
+        "wisata_favorit": "Taman Nasional Lore Lindu"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [119.8907, -1.5026]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 73,
+        "nama": "Sulawesi Selatan",
+        "deskripsi": "Sulawesi Selatan adalah sebuah provinsi di Indonesia yang terletak di bagian selatan Pulau Sulawesi. Ibu kotanya adalah Makassar.",
+        "nama_gubernur": "Nurdin Abdullah",
+        "wisata_favorit": "Tanjung Bira"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [119.4371, -4.5921]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 74,
+        "nama": "Sulawesi Tenggara",
+        "deskripsi": "Sulawesi Tenggara adalah sebuah provinsi di Indonesia yang terletak di bagian tenggara Pulau Sulawesi. Ibu kotanya adalah Kendari.",
+        "nama_gubernur": "Ali Mazi",
+        "wisata_favorit": "Pulau Labengki"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [122.9286, -4.0850]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi":
+          75,
+        "nama": "Gorontalo",
+        "deskripsi": "Gorontalo adalah sebuah provinsi di Indonesia yang terletak di bagian utara Pulau Sulawesi. Ibu kotanya adalah Gorontalo.",
+        "nama_gubernur": "Rusli Habibie",
+        "wisata_favorit": "Danau Limboto"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [123.0568, 0.7101]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 76,
+        "nama": "Sulawesi Barat",
+        "deskripsi": "Sulawesi Barat adalah sebuah provinsi di Indonesia yang terletak di bagian barat Pulau Sulawesi. Ibu kotanya adalah Mamuju.",
+        "nama_gubernur": "Ali Baal Masdar",
+        "wisata_favorit": "Pantai Pasi"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [119.3545, -2.6696]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 81,
+        "nama": "Maluku",
+        "deskripsi": "Maluku adalah sebuah provinsi di Indonesia yang terletak di bagian timur Indonesia. Ibu kotanya adalah Ambon.",
+        "nama_gubernur": "Murad Ismail",
+        "wisata_favorit": "Pulau Banda"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [128.1817, -3.6954]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 82,
+        "nama": "Maluku Utara",
+        "deskripsi": "Maluku Utara adalah sebuah provinsi di Indonesia yang terletak di bagian utara Kepulauan Maluku. Ibu kotanya adalah Sofifi.",
+        "nama_gubernur": "Khofifah Indar Parawansa",
+        "wisata_favorit": "Taman Laut Togean"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [126.8326, 1.3541]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 91,
+        "nama": "Papua Barat",
+        "deskripsi": "Papua Barat adalah sebuah provinsi di Indonesia yang terletak di bagian barat Pulau Papua. Ibu kotanya adalah Manokwari.",
+        "nama_gubernur": "Dominggus Mandacan",
+        "wisata_favorit": "Raja Ampat"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [134.1833, -1.3361]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kode_provinsi": 94,
+        "nama": "Papua",
+        "deskripsi": "Papua adalah sebuah provinsi di Indonesia yang terletak di bagian timur Pulau Papua. Ibu kotanya adalah Jayapura.",
+        "nama_gubernur": "Lukas Enembe",
+        "wisata_favorit": "Lembah Baliem"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [140.7010, -2.5489]
+      }
+    }
+  ]
+}
